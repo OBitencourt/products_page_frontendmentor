365ebb8cb1a899e532ef145f44821313822ce7c0
Arthur Bitencourt <arthurbitencourtvsilva@gmail.com>
2024-09-17 23:21:34 +0000

    adding fetch and building dom

--- main.js
+++ main.js
@@ -2,16 +2,49 @@
 
 const Main = {
 
+    
     init: function() {
-
+        this.buildCards()
+        this.cacheSelectors()
+        this.bindEvents()
     },
 
-    cacheSelector: function() {
-
+    cacheSelectors: function() {
+        this.$everyoneContainer = document.querySelector('.everyone')
     },
 
     bindEvents: function() {
 
+    },
+    
+    buildCards: function () {
+
+        fetch('./data.json').then(response => {
+            response.json().then(data => {
+                let everyone = ''
+
+                data.forEach(item => {
+                    everyone += 
+                    `
+                        <div class="product">
+                            <img src="${item.image.desktop}" alt="">
+                            <div class="button">
+                
+                            <img src="assets/images/icon-add-to-cart.svg" alt="">
+                            <h4>Add to cart</h4>
+                
+                            </div>
+                
+                            <span>${item.category}</span>
+                            <h3>${item.name}</h3>
+                            <span>R$${item.price}</span>
+                        </div>
+                    `
+                })
+
+                this.$everyoneContainer.innerHTML = everyone
+            })
+        })
     },
 
     Events: {
